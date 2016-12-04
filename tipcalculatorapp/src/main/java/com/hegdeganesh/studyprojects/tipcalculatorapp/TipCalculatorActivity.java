f537a0d75ba5dd0ed7dddb1a6180fc2ec644e09c
hegde10122 <hegde10122@gmail.com>
2016-12-04 22:24:51 +0530

    changes
    app

--- tipcalculatorapp/src/main/java/com/hegdeganesh/studyprojects/tipcalculatorapp/TipCalculatorActivity.java
+++ tipcalculatorapp/src/main/java/com/hegdeganesh/studyprojects/tipcalculatorapp/TipCalculatorActivity.java
@@ -1,9 +1,9 @@
 package com.hegdeganesh.studyprojects.tipcalculatorapp;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle;//for saving state information
 
-public class TipCalculatorActivity extends AppCompatActivity {
+public class TipCalculatorActivity extends AppCompatActivity {//calculates a bill total based on a tip percentage
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +27,16 @@ public class TipCalculatorActivity extends AppCompatActivity {
 * 4) use inheritance to create a class that defines the TipCalculator's functionality
 * 5) Use event handling,anonymous inner classes and interfaces to process the user's GUI
     interactions.
+* */
+
+/*
+* Class Bundle stores key-value pairs of information - typically representing an app's state or data that needs to be passed between activities.
+* When another app is about to appear on the screen - example, when the user receives a phone call or launches another app - Android
+* gives the currently executing app the opportunity to save its state in a Bundle. The Android runtime might subsequently kill the app - example,
+* to reclaim the memory.When the app returns to the screen, the Android runtime passes the Bundle of the previously saved state to Activity
+* method onCreate. Then the app can use the saved state to return the app to the state it was in when another app became active.
+*
+* Class AppCompatActivity provides the basic lifecycle methods of an app. AppCompatActivity is an indirect class of Activity that supports
+* using newer android features apps running on current and older Android platforms.
 * */
 
